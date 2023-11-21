ef36e1ce1ae859c2e8f7c355c6c975227b57e223
AnReyX <golubin.ru@gmail.com>
2023-11-21 19:46:24 +0200

    Changed to making circles

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def print_git():
-    print(f'My first git-repo')
+import sys
+from random import randint
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Suprematism(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.qp = QPainter()
+        self.flag = False
+        self.button = QPushButton('Сделать круги', self)
+        self.button.clicked.connect(self.drawf)
+        self.button.resize(100, 100)
+        self.button.move(250, 250)
+
+    def initUI(self):
+        self.setGeometry(400, 100, 500, 500)
+        self.setWindowTitle('Кружочки owo')
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def drawf(self):
+        self.flag = True
+        self.update()
+
+    def draw(self):
+        R = randint(20, 100)
+        coords = [randint(100, 400) for _ in range(2)]
+        self.qp.setBrush(QColor(*[255, 255, 0]))
+        self.qp.drawEllipse(int(coords[0] - R / 2), int(coords[1] - R / 2), R, R)
 
 
 if __name__ == '__main__':
-    print_git()
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec())
